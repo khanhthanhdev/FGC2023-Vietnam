5f977acda95de04c8a4b21668ed0360c9a29aa76
khanhthanhdev <thanhkt27507@gmail.com>
2023-08-05 23:37:04 +0700

    using encoder correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VNRobot/FunctionalRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VNRobot/FunctionalRobot.java
@@ -11,35 +11,62 @@ import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
 
 public class FunctionalRobot {
 
-    private IMU imu;
+//    private IMU imu;
     private Drivebase drivebase;
     private Telemetry telemetry;
     private Gamepad gamepad;
-    private Intake intake;
-    private Shooter shooter;
+//    private Intake intake;
+//    private Shooter shooter;
     private  boolean shooterState = false;
 
     public FunctionalRobot (OpMode opMode){
-        imu = new IMU(opMode);
+//        imu = new IMU(opMode);
         drivebase = new Drivebase(opMode);
-        intake = new Intake(opMode);
-        shooter = new Shooter(opMode);
+//        intake = new Intake(opMode);
+//        shooter = new Shooter(opMode);
 
         telemetry = opMode.telemetry;
         gamepad = opMode.gamepad1;
     }
 
     public void init(){
-        imu.init();
+//        imu.init();
         drivebase.init();
-        intake.init();
-        shooter.init();
+//        intake.init();
+//        shooter.init();
     }
 
     public void runOpMode(){
-        double left = -gamepad.left_stick_y;
-        double right = -gamepad.right_stick_y;
+        double left = gamepad.left_stick_y;
+        double right = gamepad.right_stick_y;
         double intakePower = 0;
+        boolean reverseState = false;
+
+        if (gamepad.touchpad){
+            reverseState = !reverseState;
+        }
+
+        if (reverseState){
+            left = -gamepad.left_stick_y;
+            right = -gamepad.right_stick_y;
+            reverseState = true;
+        } else {
+            left = gamepad.left_stick_y;
+            right = gamepad.right_stick_y;
+            reverseState = false;
+        }
+
+//        if (gamepad.left_bumper){
+//                left = left;
+//                right = right;
+//
+//        }else if (gamepad.right_bumper){
+//                left = -left;
+//                right = -right;
+//        }
+
+
+
 
         if (gamepad.triangle){
             intakePower = 1;
@@ -58,8 +85,9 @@ public class FunctionalRobot {
             shooterState = false;
         }
 
+
         drivebase.setMotorPower(left,right);
-        intake.setMotorPower(intakePower);
+//        intake.setMotorPower(intakePower);
 
         telemetry.addData("Shooter is calibrating", shooterState);
         telemetry.update();
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/BoilerplateAuto.java
@@ -0,0 +1,160 @@
+
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import java.util.ArrayList;
+
+
+@Autonomous(name = "Simple Park Auto (STRIPPED NO CONE)", group = "!Beta Dogs Auto")
+public class BoilerplateAuto extends LinearOpMode
+{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+    private BNO055IMU imu = null;
+
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C270 at default res
+    // You will need to do your own calibration for other configurations!
+    double fx = 985.4318958505312;
+    double fy = 982.8510144217059;
+    double cx = 353.64477664507393;
+    double cy = 263.136868520393;
+
+    // UNITS ARE METERS
+    double tagsize = 0.04;
+
+    int c = 0;
+
+    ArrayList<Integer> TARGET_TAGS = new ArrayList<Integer>(3);
+
+    AprilTagDetection tagOfInterest = null;
+
+    double robotHeading = 0;
+    Orientation lastAngles = new Orientation();
+
+    double getAngle() {
+        Orientation currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double deltaAngle = currentAngle.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        robotHeading += deltaAngle;
+
+        lastAngles = currentAngle;
+        return robotHeading;
+    }
+
+    @Override
+    public void runOpMode()
+    {
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+        parameters.mode                 = BNO055IMU.SensorMode.IMU;
+        parameters.accelUnit            = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled       = false;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        TARGET_TAGS.add(1);
+        TARGET_TAGS.add(2);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        telemetry.addLine("Init. Waiting for start.");
+        telemetry.update();
+        waitForStart();
+
+        telemetry.addLine("Getting latest detections");
+        telemetry.update();
+        ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+        telemetry.addLine("Got latest detections");
+        telemetry.update();
+
+        sleep(1000);
+
+        while (tagOfInterest == null && opModeIsActive())
+        {
+            currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+            if (currentDetections.size() > 0)
+            {
+                for (AprilTagDetection tag : currentDetections)
+                {
+                    if (TARGET_TAGS.contains(tag.id))
+                    {
+                        tagOfInterest = tag;
+                        break;
+                    }
+                }
+            }
+            sleep(50);
+        }
+
+        if (tagOfInterest.id == 1)
+        {
+            // drive to the moon
+        }
+        else if (tagOfInterest.id == 2)
+        {
+            // drill hole to china
+        }
+    }
+}
